a354ce548ed28cbc0b69d86ca5684794ab5696e3
lokesh bansal <lokesh@75way.com>
2020-11-19 19:41:53 +0530

    setup https and redux structure

---
+++ redux/user/actions.js
@@ -0,0 +1,13 @@
+/**
+ * Import Dependencies
+ */
+import { SET_USER } from './constants';
+
+/**
+ * Set user
+ *
+ * @param {Object} data
+ */
+export const setUser = (data = {}) => {
+  return { type: SET_USER, data };
+};
