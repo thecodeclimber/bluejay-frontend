5c37dd983466e122c6d1f7616295c9790c17a18e
kirti75waytech <kirti75waytech@gmail.com>
2020-12-16 18:23:28 +0530

    removed commented code

--- components/product/productDetail/knowYourBolt/index.js
+++ components/product/productDetail/knowYourBolt/index.js
@@ -30,32 +30,6 @@ const KnowYourBolt = () => {
   ];
 
   return (
-    // <div className="bg-light font-ubuntu">
-    //   <div className="container mx-auto py-16">
-    //     <div className="flex items-center justify-center pb-12 ">
-    //       <div className="text-3xl text-dark font-medium whitespace-pre">
-    //         Know Your Bolt
-    //       </div>
-    //     </div>
-    //     <div className="container mx-auto flex ">
-    //       {data.boltCard.map((data, index) => (
-    //         <div
-    //           key={index}
-    //           className="font-ubuntu text-center mr-1 py-5 px-6 rounded shadow-grey-8 cursor-pointer hover:bg-white hover:bg-opacity-none"
-    //         >
-    //           <img className="mx-auto pb-6" src={data.img} />
-    //           <div className="text-xl text-dark whitespace-pre font-medium text-center">
-    //             {data.boltOrg}
-    //           </div>
-    //           <hr className="m-6 opacity-10" />
-    //           <div className="text-center text-base whitespace-pre font-light leading-relaxed text-dark  whitespace-pre  ">
-    //             {data.description}
-    //           </div>
-    //         </div>
-    //       ))}
-    //     </div>
-    //   </div>
-    // </div>
     <div className=" bg-light font-ubuntu  py-16  bg-opacity-50">
       <div className="container mx-auto  pb-8 pb-12  text-3xl font-ubuntu text-dark text-center">
         <span className="font-medium"> Know Your Bolt</span>
